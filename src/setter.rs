f7409cdfb5f965b6f87e58bb69ca1d82fd247b8f
Bazyli Cyran <bazyli.cyran@gmail.com>
2022-10-05 12:23:17 +0200

    feat: Set wallpapers using wallpaper lib

--- src/setter.rs
+++ src/setter.rs
@@ -1,14 +1,9 @@
 use std::path::Path;
-use std::process::Command;
 
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Result};
 
 pub fn set_wallpaper<P: AsRef<Path>>(path: P) -> Result<()> {
     let abs_path = path.as_ref().canonicalize()?;
-    Command::new("feh")
-        .arg("--bg-fill")
-        .arg(abs_path.as_os_str())
-        .status()
-        .with_context(|| format!("failed to set wallpaper"))?;
-    Ok(())
+    wallpaper::set_from_path(abs_path.to_str().unwrap())
+        .or_else(|e| Err(anyhow!("could not set wallpaper: {}", e)))
 }
