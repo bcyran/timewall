4ca68f83217a07021a305267004250bb33ccbf62
Bazyli Cyran <bazyli.cyran@gmail.com>
2022-10-09 19:03:26 +0200

    feat: Implement HEIF image reading from PNG

--- src/heic.rs
+++ src/heic.rs
@@ -6,8 +6,8 @@ use std::{
 
 use anyhow::{anyhow, Result};
 use libheif_rs::{
-    check_file_type, ColorSpace, FileTypeResult, HeifContext, HeifError, Image, ImageHandle,
-    ItemId, RgbChroma,
+    check_file_type, Channel, ColorSpace, FileTypeResult, HeifContext, HeifError, Image,
+    ImageHandle, ItemId, RgbChroma,
 };
 use log::debug;
 
@@ -56,7 +56,7 @@ pub fn write_image_as_png<P: AsRef<Path>>(image: &Image, path: P) -> Result<()> 
     png_encoder.set_depth(png::BitDepth::Eight);
     let mut png_writer = png_encoder.write_header()?.into_stream_writer()?;
 
-    write_image_data(
+    write_from_padded_data(
         &mut png_writer,
         image_plane.data,
         image_plane.stride,
@@ -66,9 +66,9 @@ pub fn write_image_as_png<P: AsRef<Path>>(image: &Image, path: P) -> Result<()> 
     Ok(())
 }
 
-/// Write image data removing line padding if it's present.
+/// Write potentially padded image data, removing line padding if it's present.
 /// Read up on "image stride" if you don't get what's going on here.
-fn write_image_data<W: Write>(
+fn write_from_padded_data<W: Write>(
     writer: &mut W,
     data: &[u8],
     data_stride: usize,
@@ -90,12 +90,59 @@ fn write_image_data<W: Write>(
     Ok(())
 }
 
+/// Read HEIC `Image` from PNG file at the specified path.
+pub fn read_image_from_png<P: AsRef<Path>>(path: P) -> Result<Image> {
+    let file = File::open(path)?;
+    let decoder = png::Decoder::new(file);
+    let mut reader = decoder.read_info()?;
+    let mut png_data = vec![0; reader.output_buffer_size()];
+    let info = reader.next_frame(&mut png_data)?;
+    png_data.truncate(info.buffer_size());
+    png_data.shrink_to_fit();
+
+    let mut image = Image::new(info.width, info.height, ColorSpace::Rgb(RgbChroma::Rgb))?;
+    image.create_plane(Channel::Interleaved, info.width, info.height, 8)?;
+    let mut image_plane = image.planes_mut().interleaved.unwrap();
+
+    write_to_padded_data(
+        &mut image_plane.data,
+        &png_data,
+        image_plane.stride,
+        info.line_size,
+    )?;
+
+    Ok(image)
+}
+
+/// Write unpadded data into potentially padded container, adding the padding if necessary.
+fn write_to_padded_data<W: Write>(
+    writer: &mut W,
+    data: &[u8],
+    data_stride: usize,
+    line_length: usize,
+) -> Result<()> {
+    let padding_length = data_stride - line_length;
+    if padding_length == 0 {
+        debug!("image lines not padded");
+        writer.write_all(data)?;
+    } else {
+        debug!("image lines padded");
+        let padding = vec![0; padding_length];
+        for data_line in data.chunks(line_length) {
+            writer.write_all(&data_line)?;
+            writer.write_all(&padding)?;
+        }
+    }
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    pub fn test_write_image_data_no_padding() {
+    pub fn test_write_from_padded_data_no_padding() {
         #[rustfmt::skip]
         let data: &[u8] = &[
             1, 1, 1, 2, 2, 2,
@@ -106,13 +153,13 @@ mod tests {
         let line_length = 6;
 
         let mut result: Vec<u8> = Vec::new();
-        write_image_data(&mut result, data, data_stride, line_length).unwrap();
+        write_from_padded_data(&mut result, data, data_stride, line_length).unwrap();
 
         assert_eq!(result, data);
     }
 
     #[test]
-    pub fn test_write_image_data_with_padding() {
+    pub fn test_write_from_padded_data_with_padding() {
         #[rustfmt::skip]
         let data: &[u8] = &[
             1, 1, 1, 2, 2, 2, 0, 0, 0,
@@ -129,7 +176,47 @@ mod tests {
         ];
 
         let mut result: Vec<u8> = Vec::new();
-        write_image_data(&mut result, data, data_stride, line_length).unwrap();
+        write_from_padded_data(&mut result, data, data_stride, line_length).unwrap();
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_write_to_padded_data_no_padding() {
+        #[rustfmt::skip]
+        let data = &[
+            1, 1, 1,
+            2, 2, 2,
+            3, 3, 3
+        ];
+        let data_stride = 3;
+        let line_length = 3;
+
+        let mut result: Vec<u8> = Vec::new();
+        write_to_padded_data(&mut result, data, data_stride, line_length).unwrap();
+
+        assert_eq!(result, data);
+    }
+
+    #[test]
+    fn test_write_to_padded_data_with_padding() {
+        #[rustfmt::skip]
+        let data = &[
+            1, 1, 1,
+            2, 2, 2,
+            3, 3, 3
+        ];
+        let data_stride = 6;
+        let line_length = 3;
+        #[rustfmt::skip]
+        let expected = &[
+            1, 1, 1, 0, 0, 0,
+            2, 2, 2, 0, 0, 0,
+            3, 3, 3, 0, 0, 0,
+        ];
+
+        let mut result: Vec<u8> = Vec::new();
+        write_to_padded_data(&mut result, data, data_stride, line_length).unwrap();
 
         assert_eq!(result, expected);
     }
