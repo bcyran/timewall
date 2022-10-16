e6486c6b48e955e4ac4d69eec08e30654e92bf8a
Bazyli Cyran <bazyli.cyran@gmail.com>
2022-10-16 15:57:14 +0200

    feat: Implement dry run setter

--- src/setter.rs
+++ src/setter.rs
@@ -1,33 +1,82 @@
+use std::env;
 use std::path::Path;
 use std::process::Command;
 
 use anyhow::{anyhow, Context, Result};
+use itertools::Itertools;
 use log::debug;
 
+/// Set wallpaper to the image pointed by a given path. Use custom command if provided.
 pub fn set_wallpaper<P: AsRef<Path>>(path: P, custom_command: Option<&Vec<String>>) -> Result<()> {
+    let setter = get_setter();
     if let Some(command) = custom_command {
-        set_wallpaper_custom_command(path, command)
+        setter.set_wallpaper_custom_command(path.as_ref(), command)
     } else {
-        let abs_path = path.as_ref().canonicalize()?;
-        wallpaper::set_from_path(abs_path.to_str().unwrap())
-            .map_err(|e| anyhow!("could not set wallpaper: {}", e))
+        setter.set_wallpaper(path.as_ref())
     }
 }
 
-fn set_wallpaper_custom_command<P: AsRef<Path>>(path: P, command_str: &[String]) -> Result<()> {
-    let path_str = path.as_ref().to_str().unwrap();
-    let mut command = command_str.iter().map(|item| item.replace("%f", path_str));
-
-    let mut process_command = Command::new(command.next().unwrap());
-    process_command.args(command);
-    debug!("running custom command: {process_command:?}");
-
-    let command_status = process_command
-        .status()
-        .with_context(|| "failed to run custom command")?;
-
-    match command_status.success() {
-        true => Ok(()),
-        false => Err(anyhow!("custom command process failed")),
+fn get_setter() -> Box<dyn WallpaperSetter> {
+    match env::var("TIMEWALL_DRY_RUN") {
+        Err(_) => Box::new(DefaultSetter {}),
+        Ok(_) => Box::new(DryRunSetter {}),
     }
+}
+
+trait WallpaperSetter {
+    fn set_wallpaper(&self, path: &Path) -> Result<()>;
+    fn set_wallpaper_custom_command(&self, path: &Path, custom_command: &[String]) -> Result<()>;
+}
+
+/// Real, actual wallpaper setter.
+struct DefaultSetter;
+impl WallpaperSetter for DefaultSetter {
+    fn set_wallpaper(&self, path: &Path) -> Result<()> {
+        let abs_path = path.canonicalize()?;
+        wallpaper::set_from_path(abs_path.to_str().unwrap())
+            .map_err(|e| anyhow!("could not set wallpaper: {}", e))
+    }
+
+    fn set_wallpaper_custom_command(&self, path: &Path, command_str: &[String]) -> Result<()> {
+        let path_str = path.to_str().unwrap();
+        let expended_command = expand_command(command_str, path_str);
+        let mut command = expended_command.iter();
+
+        let mut process_command = Command::new(command.next().unwrap());
+        process_command.args(command);
+        debug!("running custom command: {process_command:?}");
+
+        let command_status = process_command
+            .status()
+            .with_context(|| "failed to run custom command")?;
+
+        match command_status.success() {
+            true => Ok(()),
+            false => Err(anyhow!("custom command process failed")),
+        }
+    }
+}
+
+/// Dry run setter, mainly for use in tests.
+/// Instead of actually setting the wallpaper, prints out the actions.
+struct DryRunSetter;
+impl WallpaperSetter for DryRunSetter {
+    fn set_wallpaper(&self, path: &Path) -> Result<()> {
+        println!("Set: {}", path.display());
+        Ok(())
+    }
+
+    fn set_wallpaper_custom_command(&self, path: &Path, command_str: &[String]) -> Result<()> {
+        let expanded_command = expand_command(command_str, path.to_str().unwrap());
+        println!("Run: {}", expanded_command.join(" "));
+        Ok(())
+    }
+}
+
+/// Replace '%f' in command with file path.
+fn expand_command(command_str: &[String], path_str: &str) -> Vec<String> {
+    command_str
+        .iter()
+        .map(|item| item.replace("%f", path_str))
+        .collect_vec()
 }
--- tests/set.rs
+++ tests/set.rs
@@ -2,9 +2,14 @@ mod common;
 
 use crate::common::timewall;
 use assert_cmd::Command;
-use assert_fs::TempDir;
-use predicates::prelude::*;
-use rstest::{fixture, rstest};
+use common::{testenv, TestEnv, EXAMPLE_SUN, EXAMPLE_TIME};
+use rstest::rstest;
+use std::path::PathBuf;
 
-#[rstest]
-fn test_set(mut timewall: Command) {}
+// #[rstest]
+// #[case(EXAMPLE_SUN.to_path_buf())]
+// #[case(EXAMPLE_TIME.to_path_buf())]
+// fn test_set(testenv: TestEnv, mut timewall: Command, #[case] input_path: PathBuf) {
+//     timewall.arg("set").arg(input_path);
+//     testenv.run(&mut timewall).success();
+// }
