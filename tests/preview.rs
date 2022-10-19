29a57011630cd725193d04a7cfe4d6c6fb06e5fe
Bazyli Cyran <bazyli.cyran@gmail.com>
2022-10-19 21:46:16 +0200

    tests: Add preview command tests

---
+++ tests/preview.rs
@@ -0,0 +1,24 @@
+mod common;
+use std::path::PathBuf;
+
+use assert_cmd::Command;
+use common::{
+    testenv, timewall, TestEnv, EXAMPLE_SUN, EXAMPLE_TIME, IMAGE_DAY, IMAGE_NIGHT,
+    IMAGE_SET_MESSAGE,
+};
+use predicates::prelude::*;
+use rstest::rstest;
+
+#[rstest]
+#[case(EXAMPLE_SUN.to_path_buf())]
+#[case(EXAMPLE_TIME.to_path_buf())]
+fn test_preview(testenv: TestEnv, mut timewall: Command, #[case] wall_path: PathBuf) {
+    timewall.arg("preview").arg(wall_path);
+
+    testenv
+        .run(&mut timewall)
+        .success()
+        .stdout(predicate::str::contains(IMAGE_SET_MESSAGE).count(2))
+        .stdout(predicate::str::contains(IMAGE_DAY).count(1))
+        .stdout(predicate::str::contains(IMAGE_NIGHT).count(1));
+}
