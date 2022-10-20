d84edc94ef6b80ff78bb9338218a33d05cf2ffec
Bazyli Cyran <bazyli.cyran@gmail.com>
2022-10-20 20:20:14 +0200

    feat: Improve CLI help messages

--- src/cli.rs
+++ src/cli.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 use clap::{Parser, Subcommand, ValueEnum};
 
-/// All-in-one tool for Apple dynamic HEIF wallpapers on GNU/Linux
+/// All-in-one tool for Apple dynamic HEIF wallpapers on GNU/Linux.
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
@@ -14,32 +14,32 @@ pub struct Args {
 /// Main action subcommands
 #[derive(Subcommand, Debug)]
 pub enum Action {
-    /// Print out image info
+    /// Print out wallpaper info
     Info {
-        /// Path to HEIF file
+        /// Path to HEIF wallpaper file
         file: PathBuf,
     },
     /// Quickly cycle through all images in the wallpaper
     Preview {
-        /// Path to HEIF file
+        /// Path to HEIF wallpaper file
         file: PathBuf,
-        /// Delay between wallpaper changes in milliseconds.
+        /// Delay between wallpaper changes in milliseconds
         #[arg(short, long, default_value_t = 500)]
         delay: u64,
         /// Repeat the preview in a loop until killed
         #[arg(short, long, action)]
         repeat: bool,
     },
-    /// Extract all images and metadata from HEIF file to a directory
+    /// Extract all images and metadata from HEIF wallpaper to a directory
     Unpack {
-        /// Path to HEIF file
+        /// Path to HEIF wallpaper file
         file: PathBuf,
-        /// Path to directory to output directory
+        /// Path to output directory
         output: PathBuf,
     },
     /// Set the wallpaper
     Set {
-        /// Path to HEIF file
+        /// Path to HEIF wallpaper file
         file: Option<PathBuf>,
         /// Run continuously and update the wallpaper as time passes
         #[arg(short, long, action)]
