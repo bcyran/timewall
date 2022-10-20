d03bae854d02662542d22f4abc78be5da8b4dd10
Bazyli Cyran <bazyli.cyran@gmail.com>
2022-10-20 20:16:04 +0200

    feat: Upgrade to clap v4.x

--- src/cli.rs
+++ src/cli.rs
@@ -4,10 +4,10 @@ use clap::{Parser, Subcommand, ValueEnum};
 
 /// All-in-one tool for Apple dynamic HEIF wallpapers on GNU/Linux
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[command(author, version, about, long_about = None)]
 pub struct Args {
     /// Action subcommand
-    #[clap(subcommand)]
+    #[command(subcommand)]
     pub action: Action,
 }
 
@@ -17,40 +17,35 @@ pub enum Action {
     /// Print out image info
     Info {
         /// Path to HEIF file
-        #[clap(parse(from_os_str))]
         file: PathBuf,
     },
     /// Quickly cycle through all images in the wallpaper
     Preview {
         /// Path to HEIF file
-        #[clap(parse(from_os_str))]
         file: PathBuf,
         /// Delay between wallpaper changes in milliseconds.
-        #[clap(short, long, default_value_t = 500)]
+        #[arg(short, long, default_value_t = 500)]
         delay: u64,
         /// Repeat the preview in a loop until killed
-        #[clap(short, long, action)]
+        #[arg(short, long, action)]
         repeat: bool,
     },
     /// Extract all images and metadata from HEIF file to a directory
     Unpack {
         /// Path to HEIF file
-        #[clap(parse(from_os_str))]
         file: PathBuf,
         /// Path to directory to output directory
-        #[clap(parse(from_os_str))]
         output: PathBuf,
     },
     /// Set the wallpaper
     Set {
         /// Path to HEIF file
-        #[clap(parse(from_os_str))]
         file: Option<PathBuf>,
         /// Run continuously and update the wallpaper as time passes
-        #[clap(short, long, action)]
+        #[arg(short, long, action)]
         daemon: bool,
         /// Use light or dark variant
-        #[clap(short, long, action, arg_enum, value_parser)]
+        #[arg(short, long, value_enum)]
         appearance: Option<Appearance>,
     },
 }
