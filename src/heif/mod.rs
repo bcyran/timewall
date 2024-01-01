a7c1526c35b90375957a4fb4094154297fe4bc96
Bazyli Cyran <bazyli.cyran@gmail.com>
2024-01-01 13:49:36 +0100

    style: Remove unused imports

--- src/heif/mod.rs
+++ src/heif/mod.rs
@@ -1,4 +1,4 @@
 mod convert;
 mod read;
-pub use convert::{unpack_images, write_image_as_png};
-pub use read::{get_images, get_xmp_metadata, validate_file};
+pub use convert::unpack_images;
+pub use read::{get_xmp_metadata, validate_file};
