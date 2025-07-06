3d7dda39ae2d1247eb1a97661aa6f2ff3d88702d
Bazyli Cyran <bazyli@cyran.dev>
2025-07-06 08:52:53 +0200

    fix: Raise HeifContext max_items and max_children_per_box security limits
    Fixes #178

--- src/heif/context.rs
+++ src/heif/context.rs
@@ -6,9 +6,10 @@ use libheif_rs::{HeifContext, SecurityLimits};
 pub fn from_file(path: impl AsRef<Path>) -> Result<HeifContext<'static>> {
     let mut heif_context = HeifContext::new().context("failed to create HEIF context")?;
 
-    // The default limit of 1000 items is too low for some wallpapers.
+    // The default limits are too low for some big wallpaper files.
     let mut security_limits = SecurityLimits::new();
-    security_limits.set_max_items(2000);
+    security_limits.set_max_items(6000);
+    security_limits.set_max_children_per_box(500);
     heif_context
         .set_security_limits(&security_limits)
         .context("failed to set HEIF context security limits")?;
